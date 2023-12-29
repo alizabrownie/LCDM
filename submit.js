1b1ccbdaefbe24f737a5b09ab7fd1ec2a1255559
Aliza Brown <abrown@team.curious.comassa>
2023-12-28 21:21:54 -0500

    split css and js into separate files for easier editing

---
+++ submit.js
@@ -0,0 +1,94 @@
+function submit() {
+    //getid vars are for replacing HTML with the puedo var values
+    var getid = document.getElementById("eochoice");
+    var getid2 = document.getElementById("checkeo");
+    //puedo vars are for getting values from random inputs
+    var puedo1 = document.getElementById("choice1").value;
+    var puedo2 = document.getElementById("choice2").value;
+    var check;
+    var rnum = rand(1, 100000000);
+    function rand(min, max) {
+        var rnum = Math.floor(Math.random() * (max - min)) + min;
+        return rnum;
+    }
+    document.getElementById("rplace").innerHTML = rnum;
+    // Structure: if rnum (e/o) generate new rnum1-3, if new rnum is e/o, do 
+    // START MAIN IF STATEMENT for rnum
+    //even is choice 1 odd is 2
+    if (rnum % 2 == 0) {
+        var rnum2 = rand(1, 100000000);
+        document.getElementById("rplace2").innerHTML = rnum2;
+        if (rnum2 % 2 == 0) {
+            var rnum3 = rand(1, 100000000); document.getElementById("rplace3").innerHTML = "[ " + rnum3 + " ]";
+            if (rnum3 % 2 == 0) {
+                getid.innerHTML = puedo1;
+                getid2.innerHTML = "E 1 <br/> O 2";
+            }
+            else if (rnum3 % 2 != 0) {
+                getid.innerHTML = puedo2;
+                getid2.innerHTML = "E 2 <br/> O 1";
+            }
+
+        }
+        else if (rnum2 % 2 != 0) {
+            var rnum2 = rand(1, 100000000);
+            document.getElementById("rplace2").innerHTML = rnum2;
+            if (rnum2 % 2 == 0) {
+                var rnum3 = rand(1, 100000000);
+                document.getElementById("rplace3").innerHTML = "[ " + rnum3 + " ]";
+                if (rnum3 % 2 == 0) {
+                    getid.innerHTML = puedo1;
+                    getid2.innerHTML = "E 1 <br/> O 2";
+                }
+                else if (rnum3 % 2 != 0) {
+                    getid.innerHTML = puedo2;
+                    getid2.innerHTML = "E 2 <br/> O 1";
+                }
+            }
+            else if (rnum2 % 2 != 0) {
+                var rnum3 = rand(1, 100000000);
+                document.getElementById("rplace3").innerHTML = "[ " + rnum3 + " ]";
+                if (rnum3 % 2 == 0) {
+                    getid.innerHTML = puedo1;
+                    getid2.innerHTML = "E 1 <br/> O 2";
+                }
+                else if (rnum3 % 2 != 0) {
+                    getid.innerHTML = puedo2;
+                    getid2.innerHTML = "E 2 <br/> O 1";
+                }
+            }
+        }
+        // MAIN ELSE STATEMENT for rnum
+    }
+    else {
+        var rnum2 = rand(1, 100000000);
+        document.getElementById("rplace2").innerHTML = rnum2;
+        if (rnum2 % 2 == 0) {
+            var rnum3 = rand(1, 100000000);
+            document.getElementById("rplace3").innerHTML = "{ " + rnum3 + " }";
+            if (rnum3 % 2 == 0) {
+                getid.innerHTML = puedo1;
+                getid2.innerHTML = "E 1 <br/> O 2";
+            }
+            else if (rnum3 % 2 != 0) {
+                getid.innerHTML = puedo2;
+                getid2.innerHTML = "E 2 <br/> O 1";
+            }
+        }
+        else if (rnum2 % 2 != 0) {
+            var rnum3 = rand(1, 100000000);
+            document.getElementById("rplace3").innerHTML = "{ " + rnum3 + " }";
+            if (rnum3 % 2 == 0) {
+                getid.innerHTML = puedo1;
+                getid2.innerHTML = "E 1 <br/> O 2";
+            }
+            else if (rnum3 % 2 != 0) {
+                getid.innerHTML = puedo2;
+                getid2.innerHTML = "E 2 <br/> O 1";
+            }
+        }
+    }
+    document.getElementById("button").innerHTML = "Rediscover Your Doom (Try Again)";
+}
+//HELPFUL TIP -- [else] & {if}
+//O -> e2 & E -> e1
